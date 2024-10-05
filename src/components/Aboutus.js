20d831c49870c0c6da5d7c3e3c3ab7698dc7391f
Praveen-Thathsara <praveenstudy823@gmail.com>
2024-10-05 13:40:28 +0530

    changed some components

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -3,19 +3,19 @@ import React from 'react';
 const Aboutus = () => {
   return (
     <>
-    <h1>Jeew aiya</h1>
-    <p>22CIS0312 - Thilini
-22CIS0243 - Malith
-22CIS0279 - Jeew
-22CIS0282 - Praveen
-22CIS0303 - Isurika
-22CIS0308 - Chathurani
-22CIS0325 - Janidu
-22FIS0459 - Wathsala
-22FIS0547 - Dilshani
-22FIS0551 - Kawshalya 
-22FIS0573 - Asvinitha
-22FIS0581 - Mathurya
+    <h1>Our Team</h1><br></br>
+    <p>22CIS0312 - Thilini<br></br>
+22CIS0243 - Malith<br></br>
+22CIS0279 - Jeew<br></br>
+22CIS0282 - Praveen<br></br>
+22CIS0303 - Isurika<br></br>
+22CIS0308 - Chathurani<br></br>
+22CIS0325 - Janidu<br></br>
+22FIS0459 - Wathsala<br></br>
+22FIS0547 - Dilshani<br></br>
+22FIS0551 - Kawshalya <br></br>
+22FIS0573 - Asvinitha<br></br>
+22FIS0581 - Mathurya<br></br>
     </p>
     </>
     
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,7 @@ const Footer = () => {
     <p align="center">
     &copy; 2024 SpotOn: Your Travel Picks
     </p>
-    <p align="center">22CIS0312 - Thilini
- | 22CIS0243 - Malith
- | 22CIS0279 - Jeew
- | 22CIS0282 - Praveen<br></br>
-22CIS0303 - Isurika
- | 22CIS0308 - Chathurani
- | 22CIS0325 - Janidu
- | 22FIS0459 - Wathsala<br></br>
-22FIS0547 - Dilshani
- | 22FIS0551 - Kawshalya 
- | 22FIS0573 - Asvinitha
- | 22FIS0581 - Mathurya</p>
+
     </>
     
   );
